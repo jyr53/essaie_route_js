3157e5df76c0ed08b64a35f987b6d10494062d82
jyr <jean-yves.rouseau@orange.fr>
2022-08-30 14:03:19 +0200

    debut des routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ramdomView from '../views/ramdomView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/ramdom',
+    name: 'ramdom',
+    component: ramdomView
   },
   {
     path: '/about',
---
+++ src/views/ramdomView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div> <a v-on;click="gotToPage">{{  numeroAleatoire  }} bonjour</a></div><br>
+    {{  $router  }}
+
+
+</template>
+<script>
+
+
+
+export default {
+    name: 'ramdomView',
+    computed: {
+        numeroAleatoire() {
+            return Math.floor(Math.random() * 100);
+        },
+
+    },
+    methods: {
+        goToPage() {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
+
+<style>
+</style>
